34613ceb54127bd762ea60276e87cd691db7b36d
Jack-Hann <103619881@student.swin.edu.au>
2022-04-27 09:48:22 +1000

    PHP Inclues
    Added .inc files to replace repetitive code such as <header> and <footer>

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="An introduction to the Internet of Things">
+  <meta name="keywords"    content="IoT, Internet of Things">
+  <meta name="author"      content="Group 3">
+  <meta name="viewport"    content="width=device-width, initial-scale=1.0"> <!-- For a responsive website -->
+  <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">--> <!-- Placeholder stylesheet -->
+  <link rel="stylesheet" href="styles/style.css">
+  <title>IoT - Internet of Things</title>
+</head>
+<body id=intro>
+  <?php include 'header.inc';?>
+  <br>
+  <section id="summary">
+    <h1>IoT - Internet of Things</h1>
+    <p>Internet of Things (IoT) describes the interconnection and communication of a network of 'things' which can work together to perform functions and exchange data between each other. In this case 'things' refer to any device that can connect to and communicate within a network.</p>
+    <p>The connection of these devices can streamline, and therefore enhance, our current way of life. IoT has a vast and varied number of uses and can be applied to nearly any circumstance. IoT is evident in small, interconnected household appliances such as smart lights and smart speakers which can all be automated through devices such as Google Home or Amazon Echo. However, IoT can also be found on a much larger scale with the emergence of completely interconnected 'Smart Cities'.</p>   
+  </section>	
+  <?php include 'footer.inc';?>
+</body>
+</html>
