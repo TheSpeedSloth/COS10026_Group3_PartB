e24f5e9a9f0889b5f6bb3b79f16ff9a238802812
LT-103985456 <103541180+LT-103985456@users.noreply.github.com>
2022-05-25 09:03:11 +1000

    Marking implemented

--- markquiz.php
+++ markquiz.php
@@ -1,54 +1,61 @@
 <?php
-require("classes/attempts.php");
+require_once("classes/attempt.php");
+require_once("settings.php");
 
 if(isset($_POST["submit"])) {
-    // Get inputs from POST array
-    $studentID     = $_POST["studentID"];
-    $givenName     = $_POST["givenName"];
-    $familyName    = $_POST["familyName"];
-    $history1      = $_POST["history1"];
-    $history2      = $_POST["history2"];
-    $history3      = $_POST["history3"];
-    $smartCity1    = $_POST["smartCity1"];
-    $smartCity2    = $_POST["smartCity2"];
-    $smartCity3    = $_POST["smartCity3"];
-    $smartVehicle1 = $_POST["smartVehicle1"];
-    $smartVehicle2 = $_POST["smartVehicle2"];
-    $smartVehicle3 = $_POST["smartVehicle3"];
-    $smartVehicle4 = $_POST["smartVehicle4"];
-    $smartVehicle5 = $_POST["smartVehicle5"];
-    $iotVul1       = $_POST["iotVul1"];
-    $iotVul2       = $_POST["iotVul2"];
+    // Connect to MySQL database
+    $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+    if($conn) {
+        // Get inputs from POST array
+        $studentID     = $_POST["studentID"];
+        $givenName     = $_POST["givenName"];
+        $familyName    = $_POST["familyName"];
+        $history1      = $_POST["history1"];
+        $history2      = $_POST["history2"];
+        $history3      = $_POST["history3"];
+        $smartCity1    = $_POST["smartCity1"];
+        $smartCity2    = $_POST["smartCity2"];
+        $smartCity3    = $_POST["smartCity3"];
+        $smartVehicle1 = $_POST["smartVehicle1"];
+        $smartVehicle2 = $_POST["smartVehicle2"];
+        $smartVehicle3 = $_POST["smartVehicle3"];
+        $smartVehicle4 = $_POST["smartVehicle4"];
+        $smartVehicle5 = $_POST["smartVehicle5"];
+        $iotVul1       = $_POST["iotVul1"];
+        $iotVul2       = $_POST["iotVul2"];
 
-    // Create answers array
-    $answers = [];
-    $answers[] = $studentID;
-    $answers[] = $givenName;
-    $answers[] = $familyName;
-    $answers[] = $history1;
-    $answers[] = $history2;
-    $answers[] = $history3;
-    $answers[] = $smartCity1;
-    $answers[] = $smartCity2;
-    $answers[] = $smartCity3;
-    $answers[] = $smartVehicle1;
-    $answers[] = $smartVehicle2;
-    $answers[] = $smartVehicle3;
-    $answers[] = $smartVehicle4;
-    $answers[] = $smartVehicle5;
-    $answers[] = $iotVul1;
-    $answers[] = $iotVul2;
+        // Create answers array
+        $answers = [];
+        $answers[] = $studentID;
+        $answers[] = $givenName;
+        $answers[] = $familyName;
+        $answers[] = $history1;
+        $answers[] = $history2;
+        $answers[] = $history3;
+        $answers[] = $smartCity1;
+        $answers[] = $smartCity2;
+        $answers[] = $smartCity3;
+        $answers[] = $smartVehicle1;
+        $answers[] = $smartVehicle2;
+        $answers[] = $smartVehicle3;
+        $answers[] = $smartVehicle4;
+        $answers[] = $smartVehicle5;
+        $answers[] = $iotVul1;
+        $answers[] = $iotVul2;
 
-    $attempt = new Attempt($answers); // Some OOP don't worry about it
-    $error = $attempt->validate();
-    if(empty($error)) {
-        // Marking code goes here
+        $attempt = new Attempt($answers);
+        $error = $attempt->validate();
+        if(empty($error)) {
+            $score = $attempt->mark();
+        } else {
+            echo $error;
+        }
+        echo "<pre>";
+        print_r($attempt);
+        echo "</pre>";
     } else {
-        echo $error;
+        echo "Database could not be accessed.";
     }
-    echo "<pre>";
-    print_r($attempt);
-    echo "</pre>";
 } else {
     header("Location: index.php");
 }
---
+++ settings.php
@@ -0,0 +1,9 @@
+<?php
+
+$host = "feenix-mariadb.swin.edu.au";
+$user = "s0000000000";
+$pwd = "000000";
+$sql_db = "s0000000000_db";
+$conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+?>
